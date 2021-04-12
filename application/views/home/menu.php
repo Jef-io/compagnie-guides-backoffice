0b366cd59f3823961da190186cbe2f8d45c2a1a8
Maia <maia.dasilva.12@gmail.com>
2021-04-12 14:00:57 +0200

    Revert "test menu déroulant"
    This reverts commit 6454c38ba1ffb9f43a3d0891dbfc08f2eac226f1.

--- application/views/home/menu.php
+++ application/views/home/menu.php
@@ -6,13 +6,7 @@
   echo '<nav>';
   echo '<h2>Consulter :</h2>' ;
   echo '<ul>' ;
-  echo '<li>
-          Abris
-          <ul>
-            <li><a href="'.$home.'index.php/abris">Les abris</a></li>
-            <li><a href="'.$home.'index.php/abris/creer">Ajouter un abri</a></li>
-          </ul>
-        </li>';
+  echo '<li><a href="'.$home.'index.php/abris">Les abris</a></li>';
   echo '<li><a href="'.$home.'index.php/guides">Les guides</a></li>';
   echo '<li><a href="'.$home.'index.php/sommets">Les sommets</a></li>';
   echo '<li><a href="'.$home.'index.php/vallees">Les vallées</a></li>';
--- application/views/sommets/creer.php
+++ application/views/sommets/creer.php
@@ -3,12 +3,13 @@
 
   echo form_open('sommets/creer'); //Ouvre la balise form et crée la cible vers la route employes/creer
 ?>
-
-    <label for="nom_Sommets">Nom</label>
-    <input type="text" name="nom_Sommets" required/>
-
-    <label for="altitude_Sommets">Altitude</label>
-    <input type="text" name="altitude_Sommets" required/>
-
+    <div>
+      <label for="nom_Sommets">Nom</label>
+      <input type="text" name="nom_Sommets" required/>
+    </div>
+    <div>
+      <label for="altitude_Sommets">Altitude</label>
+      <input type="text" name="altitude_Sommets" required/>
+    </div>
     <input type="submit" name="submit" value="Créer un sommet" />
 </form>
