8e3a1b3e8ebe478027e2d9afb0220af1816b3551
Maia <maia.dasilva.12@gmail.com>
2021-04-12 10:35:25 +0200

    Update Abris_model.php

--- application/models/Abris_model.php
+++ application/models/Abris_model.php
@@ -19,11 +19,11 @@ class Abris_model extends CI_Model {
         return $query->result();
     }
 
-    public function create($code_Abris, $nom_Abris, $type_Abris, $altitude_Abris, $places_Abris, 
-                           $prixNuit_Abris, $prixRepas_Abris, $telGardien_Abris){
-        $data = array('code_Abris'=>$code_Abris 'nom_Abris'=>$nom_Abris, 'type_Abris'=>$type_Abris, 
+    public function create($nom_Abris, $type_Abris, $altitude_Abris, $places_Abris, 
+                           $prixNuit_Abris, $prixRepas_Abris, $telGardien_Abris, $code_Vallees){
+        $data = array('nom_Abris'=>$nom_Abris, 'type_Abris'=>$type_Abris, 
                       'altitude_Abris'=>$altitude_Abris, 'places_Abris'=>$places_Abris, 'prixNuit_Abris'=>$prixNuit_Abris,
-                      'prixRepas_Abris'=>$prixRepas_Abris, 'telGardien_Abris'=>$telGardien_Abris););
+                      'prixRepas_Abris'=>$prixRepas_Abris, 'telGardien_Abris'=>$telGardien_Abris, 'code_Vallees'=>$code_Vallees););
         return $this->db->insert('abris', $data);
     }
 }
