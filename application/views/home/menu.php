1c1e924b7f2f20dcf36be1ad55299a04806e3daf
Maia <maia.dasilva.12@gmail.com>
2021-04-12 10:11:36 +0200

    Update menu.php

--- application/views/home/menu.php
+++ application/views/home/menu.php
@@ -1,18 +1,23 @@
 <?php
-  echo '<h2>'.$titre.'</h2>';
+  echo '<h1>'.$titre.'</h1>';
 
   $home='http://localhost:80/RTAI/compagnie-guides-backoffice/';
 
+  echo '<h2>Consulter</h2>' ;
   echo '<ul>' ;
   echo '<li><a href="'.$home.'index.php/abris">Les abris</a></li>';
-  echo '<li><a href="'.$home.'index.php/abris/creer">Ajouter un abri</a></li>';
   echo '<li><a href="'.$home.'index.php/guides">Les guides</a></li>';
-  echo '<li><a href="'.$home.'index.php/guides/creer">Ajouter un guide</a></li>';
   echo '<li><a href="'.$home.'index.php/sommets">Les sommets</a></li>';
-  echo '<li><a href="'.$home.'index.php/sommets/creer">Ajouter un sommet</a></li>';
   echo '<li><a href="'.$home.'index.php/vallees">Les vallées</a></li>';
-  echo '<li><a href="'.$home.'index.php/vallees/creer">Ajouter une vallée</a></li>';
   echo '<li><a href="'.$home.'index.php/ascensions">Les ascensions</a></li>';
+  echo '</ul>' ;
+
+  echo '<h2>Ajouter</h2>' ;
+  echo '<ul>' ;
+  echo '<li><a href="'.$home.'index.php/abris/creer">Ajouter un abri</a></li>';
+  echo '<li><a href="'.$home.'index.php/guides/creer">Ajouter un guide</a></li>';
+  echo '<li><a href="'.$home.'index.php/sommets/creer">Ajouter un sommet</a></li>';
+  echo '<li><a href="'.$home.'index.php/vallees/creer">Ajouter une vallée</a></li>';
   echo '<li><a href="'.$home.'index.php/ascensions/creer">Ajouter une ascension</a></li>';
   echo '</ul>' ;
 
