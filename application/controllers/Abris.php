caba0eae0ea5891dbc02773018f150248806afa1
Maia <maia.dasilva.12@gmail.com>
2021-04-13 09:28:04 +0200

    fix modifier

--- application/controllers/Abris.php
+++ application/controllers/Abris.php
@@ -95,12 +95,19 @@ class Abris extends CI_Controller {
       $prixRepas_Abris = $this->input->post('prixRepas_Abris');
       $telGardien_Abris = $this->input->post('telGardien_Abris');
       $code_Vallees = $this->input->post('code_Vallees');
-      $this->abris_model->create($nom_Abris, $type_Abris, $altitude_Abris, $places_Abris, $prixNuit_Abris, 
+      $this->abris_model->update($id, $nom_Abris, $type_Abris, $altitude_Abris, $places_Abris, $prixNuit_Abris, 
                                   $prixRepas_Abris, $telGardien_Abris, $code_Vallees);
 
-        redirect('/abris', 'refresh');
+      redirect('/abris', 'refresh');
     } else {
-        $data['abris'] = $this->abris_model->find($id);
+        $abris = $this->abris_model->find($id);
+        $vallees = array();
+        foreach($abris as $abri){
+          $vallees[$abri->code_Abris]['nom_Vallees'] = $this->vallees_model->find($abri->code_Vallees);
+        }
+
+        $data['abris'] = $abris;
+        $data['vallees'] = $vallees;
 
         $this->load->view('header');
         $this->load->view('abris/modifier', $data);
--- application/views/ascensions/modifier.php
+++ application/views/ascensions/modifier.php
@@ -7,32 +7,44 @@
     echo '<label for="code_Sommets">Sommet</label>
     <select name="code_Sommets" required>
       <option value="">--Please choose an option--</option>' ;
+      $value = $ascension->code_Sommets ;
       foreach($sommets as $sommet){
-        echo '<option value="'.$sommet->code_Sommets.'">'.$sommet->nom_Sommets.'</option>' ;
+        if ($sommet->code_Sommets == $value) {
+          echo '<option value="'.$sommet->code_Sommets.'" selected>'.$sommet->nom_Sommets.'</option>' ;
+        } else {
+          echo '<option value="'.$sommet->code_Sommets.'">'.$sommet->nom_Sommets.'</option>' ;
+        }
       }
     echo '</select>' ;
 
     echo '<label for="code_Abris">Abris</label>
     <select name="code_Abris" required>
       <option value="">--Please choose an option--</option>' ;
+      $value = $ascension->code_Abris ;
       foreach($abris as $abri){
-        echo '<option value="'.$abri->code_Abris.'">'.$abri->nom_Abris.'</option>' ;
+        if ($abri->code_Abris == $value) {
+          echo '<option value="'.$abri->code_Abris.'" selected>'.$abri->nom_Abris.'</option>' ;
+        } else {
+          echo '<option value="'.$abri->code_Abris.'">'.$abri->nom_Abris.'</option>' ;
+        }
       }
     echo '</select>' ;
 
     echo '<label for="difficulte_Ascension">Niveau de difficulté</label>
     <select name="difficulte_Ascension" required>
-      <option value="">--Please choose an option--</option>
-      <option value="debutant"'.if ($ascension->difficulte_Ascension == 'debutant') { echo 'selected'}.'>débutant</option>
-      <option value="confirmé"'.if ($ascension->difficulte_Ascension == 'confirmé') { echo 'selected'}.'>confirmé</option>
-      <option value="expert"'.if ($ascension->difficulte_Ascension == 'expert') { echo 'selected'}.'>expert</option>
-    </select>' ;
+      <option value="">--Please choose an option--</option>' ;
+      $value = $ascension->difficulte_Ascension ;
+      if ($value == 'debutant') {
+        echo '<option value="debutant" selected>débutant</option>' ;
+      } else if ($value == 'confirmé') {
+        echo '<option value="confirmé" selected>confirmé</option>' ;
+      } else if ($value == 'expert') {
+        echo '<option value="expert" selected>expert</option>' ;
+      }
+    echo '</select>' ;
 
-    echo '<label for="difficulte_Ascension">Difficulté</label>';
-    echo '<input type="text" name="prenom" value="'.$ascension->difficulte_Ascension.'"/>';
-
-    echo '<label for="email">Email</label>';
-    echo '<input type="text" name="email" value="'.$ascension->duree_Ascension.'"/>';
+    echo '<label for="duree_Ascension">Durée</label>';
+    echo '<input type="number" name="duree_Ascension" value="'.$ascension->duree_Ascension.'"/>';
 }
 ?>
   <input type="submit" name="submit" value="Enregistrer" /><br>
